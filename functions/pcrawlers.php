c42babea883a0b3cf0b4a3753ffa26579ed94b85
Chen Ran <crccw@moonux.org>
2015-02-22 23:59:32 +0800

    change SYSU domain to IP, since domain no longer work EOM

--- functions/pcrawlers.php
+++ functions/pcrawlers.php
@@ -316,7 +316,7 @@ function pcrawler_openjudge_num() {
 }
 
 function pcrawler_sysu($pid) {
-    $url="http://soj.me/$pid";
+    $url="http://222.200.185.45//$pid";
     $content=file_get_contents($url);
     $ret=array();
 
