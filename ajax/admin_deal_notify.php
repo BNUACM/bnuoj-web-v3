44c698dfb7ad31022c54c1910b3a9d2d411c0087
Chen Ran <crccw@moonux.org>
2015-04-13 20:56:46 +0800

    Fix: announcement cannot be changed

--- ajax/admin_deal_notify.php
+++ ajax/admin_deal_notify.php
@@ -5,7 +5,7 @@ $ret=array();
 $ret["code"]=1;
 if ($current_user->is_root()) {
     $sub = convert_str($_POST['sub']);
-    $sql_up_con = "update config set substitle='$sub' where lable=1";
+    $sql_up_con = "update config set value='$sub' where name='substitle'";
     $db->query($sql_up_con);
     $ret["code"]=0;
     $ret["msg"]="Success!";
